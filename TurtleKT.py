20adbee977dd7982f8cedf070484ebd6a7089c4f
Kert-Andero <kertandero@hkhk.edu.ee>
2025-05-17 22:08:02 +0300

    Add files via upload

---
+++ TurtleKT.py
@@ -0,0 +1,60 @@
+#Kert-andero Põldmaa
+#Turtle KT
+import turtle
+
+
+#2
+for i in range(4):
+    turtle.fd(50)
+    turtle.lt(90)
+turtle.penup()
+turtle.fd(25)
+turtle.rt(90)
+turtle.fd(10)
+turtle.rt(225)
+turtle.pendown()
+for i in range(4):
+    turtle.fd(50)
+    turtle.left(90)
+
+turtle.penup()
+turtle.lt(15)
+turtle.fd(150)
+turtle.pendown()
+
+#4
+for i in range(5):
+    turtle.rt(75)
+    turtle.fd(25)
+    turtle.lt(90)
+    turtle.fd(50)
+    turtle.lt(90)
+    turtle.fd(50)
+    turtle.lt(90)
+    turtle.fd(50)
+    turtle.lt(90)
+    turtle.fd(25)
+
+turtle.penup()
+turtle.lt(15)
+turtle.fd(150)
+turtle.pendown()
+
+#5
+for i in range(6):
+    turtle.lt(120)
+    turtle.fd(50)
+    turtle.lt(120)
+    turtle.fd(50)
+    turtle.lt(120)
+    turtle.fd(50)
+    turtle.lt(60)
+    turtle.fd(50)
+
+turtle.penup()
+turtle.rt(90)
+turtle.fd(150)
+turtle.pendown()
+turtle.done()
+
+
